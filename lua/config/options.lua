e0debe809dd43fd53c3a2a479bf0dbee892d89fe
Josh Elias <josh@daemontech.tools>
2026-06-08 12:37:56 -0400

    chore: misc

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.relativenumber = false
+vim.opt.wrap = true
+
+vim.g.lazyvim_rust_diagnostics = "rust-analyzer"
+vim.g.lazyvim_eslint_auto_format = false
